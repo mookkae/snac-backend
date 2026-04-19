989bcafc1f6676dba626bc0880d077fdde48ddb8
mookkae <ehsk2315@kyungsung.ac.kr>
2026-04-19 17:49:09 +0900

    [feat] ClockConfig 및 결제 취소 예외 계층 추가
    ClockConfig
    - Clock 빈 등록 (테스트 시 Clock 교체 용이성 확보)
    
    PaymentCancellationInProgressException
    - 동시 취소 요청 충돌 시 발생 (CANCEL_REQUESTED 중복 진입 차단)
    
    PaymentMethodNotCancelableException
    - 취소 불가 결제 수단 (가상계좌, 알 수 없는 수단) 시 발생
    
    UnsupportedPaymentMethodException
    - 시스템 미지원 결제 수단 사용 시 발생
    
    PaymentPeriodExpiredException
    - 취소 기간 만료 예외 에러코드 정비
    
    Related #61

--- src/main/java/com/ureca/snac/payment/exception/PaymentPeriodExpiredException.java
+++ src/main/java/com/ureca/snac/payment/exception/PaymentPeriodExpiredException.java
@@ -3,7 +3,6 @@ package com.ureca.snac.payment.exception;
 import com.ureca.snac.common.exception.BusinessException;
 
 import static com.ureca.snac.common.BaseCode.PAYMENT_PERIOD_EXPIRED;
-import static com.ureca.snac.common.BaseCode.TOSS_INVALID_CARD_INFO;
 
 public class PaymentPeriodExpiredException extends BusinessException {
     public PaymentPeriodExpiredException() {
