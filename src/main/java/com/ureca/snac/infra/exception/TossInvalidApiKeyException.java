a6449c5a75225f8a0c89660ff66009e7c0589f7f
mookkae <ehsk2315@kyungsung.ac.kr>
2026-04-19 17:40:04 +0900

    [refactor] Toss 예외 계층 infra/exception 패키지로 이동 및 정비
    TossErrorCode
    - infra/ → infra/exception/ 패키지 이동
    
    TossInvalidApiKeyException, TossInvalidCardInfoException, TossNotEnoughBalanceException
    - payment/exception/ → infra/exception/ 패키지 이동
    
    TossAlreadyCanceledPaymentException, TossAlreadyProcessedPaymentException
    TossNotCancelablePaymentException, TossPaymentNotFoundException, TossPaymentsApiCallException
    TossRetryableException
    - Toss API 응답 케이스 세분화 신규 추가
    
    Related #65

--- src/main/java/com/ureca/snac/infra/exception/TossInvalidApiKeyException.java
+++ src/main/java/com/ureca/snac/infra/exception/TossInvalidApiKeyException.java
@@ -1,11 +1,12 @@
-package com.ureca.snac.payment.exception;
+package com.ureca.snac.infra.exception;
 
 import com.ureca.snac.common.exception.ExternalApiException;
 
-import static com.ureca.snac.common.BaseCode.TOSS_INVALID_API_KEY;
+import static com.ureca.snac.common.BaseCode.PAYMENT_GATEWAY_CONFIG_ERROR;
 
 public class TossInvalidApiKeyException extends ExternalApiException {
+
     public TossInvalidApiKeyException() {
-        super(TOSS_INVALID_API_KEY);
+        super(PAYMENT_GATEWAY_CONFIG_ERROR);
     }
 }
--- src/main/java/com/ureca/snac/infra/exception/TossNotEnoughBalanceException.java
+++ src/main/java/com/ureca/snac/infra/exception/TossNotEnoughBalanceException.java
@@ -1,11 +1,12 @@
-package com.ureca.snac.payment.exception;
+package com.ureca.snac.infra.exception;
 
 import com.ureca.snac.common.exception.BusinessException;
 
-import static com.ureca.snac.common.BaseCode.TOSS_NOT_ENOUGH_BALANCE;
+import static com.ureca.snac.common.BaseCode.GATEWAY_INSUFFICIENT_BALANCE;
 
 public class TossNotEnoughBalanceException extends BusinessException {
+
     public TossNotEnoughBalanceException() {
-        super(TOSS_NOT_ENOUGH_BALANCE);
+        super(GATEWAY_INSUFFICIENT_BALANCE);
     }
 }
